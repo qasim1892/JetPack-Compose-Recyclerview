a9cdee17002a771072385cfa02c31cf514b0cf4b
Qasim Abbasi <qasimabbasi61@gmail.com>
2023-10-24 11:03:43 +0500

    Screenshots upload

--- app/src/main/java/com/aussource/jatpackcomposedemo/ProfileScreen.kt
+++ app/src/main/java/com/aussource/jatpackcomposedemo/ProfileScreen.kt
@@ -1,9 +1,45 @@
 package com.aussource.jatpackcomposedemo
 
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.aussource.jatpackcomposedemo.model.Puppy
 
 @Composable
 fun ProfileScreen(puppy: Puppy, onNavIconPressed: () -> Unit = { }) {
+    val scrollState = rememberScrollState()
+    Column(modifier = Modifier.fillMaxSize()) {
+        BoxWithConstraints(modifier = Modifier.weight(1f)) {
+            Surface {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(scrollState),
+                ) {
 
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun profileHeader(
+        scrollState: ScrollState,
+        puppy: Puppy,
+        containerHeight: Dp
+    ) {
+        val offset=(scrollState.value/2)
+            // val offsetDp=with(LocalDensity.current)(offset.toDp)
+    }
 }
